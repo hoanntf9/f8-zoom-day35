08eee4a501767d343647354aa07c94d7472e79bf
hoanntf9 <hoan03081994@gmail.com>
2025-09-01 23:20:57 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import jsconfigPaths from 'vite-jsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
-  base: '/f8-zoom-day35/',
+  base: "/f8-zoom-day35/",
 });
