d8454711b22f3a9dc80258e6749f2379dbce0ab4
hoanntf9 <hoanntf9@github.com>
2025-09-02 08:53:21 +0700

    Merge branch 'gh-pages' of https://github.com/hoanntf9/f8-zoom-day35 into gh-pages

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import '@/assets/main.scss';
-import AppRouters from "./components/AppRouters";
-
-function App() {
-  return <AppRouters />;
-}
-
-export default App;
--- src/pages/Home/index.jsx
+++
@@ -1,9 +0,0 @@
-import Navigation from "@/components/Navigation";
-
-function Home() {
-  return <>
-    <h1>Chào mừng đến với F8 React Day 35</h1>
-  </>;
-}
-
-export default Home;
